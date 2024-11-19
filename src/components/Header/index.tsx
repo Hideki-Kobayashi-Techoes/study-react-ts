c64dba08b556b8f6a27eb89b27ba3654bf17a564
h-kobayashi <h-kobayashi@techoes.co.jp>
2024-11-19 15:13:58 +0900

    ♻️ HeaderとMainとFooterをコンポーネント化

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+const NAV_ITEMS = [
+  {href: "/", label: "Index"},
+  {href: "/about", label: "About"},
+]
+
+export function Header() {
+  return (
+    <header className="row-start-1 flex gap-6 flex-wrap items-center justify-center">
+        {NAV_ITEMS.map((item) => {
+          return (
+            <Link key={item.href} href={item.href} className="hover:underline hover:underline-offset-4">
+              {item.label}
+            </Link>
+          )
+        })}
+    </header>
+  );
+}
